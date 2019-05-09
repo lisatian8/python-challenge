b4fc70988add08044b72d5ebb03f47869629fce7
lisatian8 <lt8760@yahoo.com>
2019-05-09 15:49:47 -0400

    main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,17 +7,17 @@ import csv
 #video = input("What show or movie are you looking for? ")
 
 # Lists to store data
-dates = []
-profitLosses = []
+candidates = ["Correy", "Khan", "Li", "O'Tooley"]
+votes = [0,0,0,0]
 
-totalNumOfMonths = 0
-totalPrfitLoss = 0
-maxLoss = 0
-maxLossDate = ""
-maxProfit = 0
-maxProfitDate = ""
-totalLoss = 0
-averageChange = 0
+totalVotes = 0
+#totalPrfitLoss = 0
+#maxLoss = 0
+#maxLossDate = ""
+#maxProfit = 0
+#maxProfitDate = ""
+#totalLoss = 0
+#averageChange = 0
 
 
 # Set path for file
@@ -38,45 +38,48 @@ with open(csvpath, newline="") as csvfile:
     # Loop through looking for the video
     for row in csvreader:
 
-        dates.append(str(row[0]))
+       totalVotes = int(totalVotes) + 1
+
+       votes[int(candidates.index(str(row[2])))] = votes[int(candidates.index(str(row[2])))]  + 1
+
        # print("here")
         #print(str(row[0]))
         ####print(f'date: {row[0]}')
 
-        profitLosses.append(float(row[1]))
+       # profitLosses.append(float(row[1]))
        # print("here")
         #print(str(row[0]))
         ####print(f'profigLosses: {row[1]}')
 
-        totalPrfitLoss = float(totalPrfitLoss) + float(row[1])
+        
 
         #if float(row[1]) > 0 && float(row[1]) > float(maxProfit):
-        if float(row[1]) > float(maxProfit):
-            maxProfit = float(row[1])
-            maxProfitDate = str(row[0])
+       # if float(row[1]) > float(maxProfit):
+           # maxProfit = float(row[1])
+          #  maxProfitDate = str(row[0])
 
-        elif float(row[1]) < float(maxLoss):
-            maxLoss = float(row[1])
-            maxLossDate = str(row[0])
-            totalLoss = totalLoss + float(row[1])
+        #elif float(row[1]) < float(maxLoss):
+            #maxLoss = float(row[1])
+           # maxLossDate = str(row[0])
+           # totalLoss = totalLoss + float(row[1])
 
-    #print(dates)
-    #print(profitLosses)
+    print(candidates)
+    print(votes)
     #print(f"{len(dates)}")
 
-    totalNumOfMonths=len(dates)
-    averageChange = float(totalPrfitLoss)/float(totalNumOfMonths)
+    #totalNumOfMonths=len(dates)
+    #averageChange = float(totalPrfitLoss)/float(totalNumOfMonths)
     #averageChange = float(totalLoss)/float(totalNumOfMonths)
     
 
-    print(f'Total Months: {totalNumOfMonths}')
-    print(f'Total: ${totalPrfitLoss}')
-    print(f'Average Change: ${averageChange}')
+    print(f'Total Votes {totalVotes}')
+    #print(f'Total: ${totalPrfitLoss}')
+    #print(f'Average Change: ${averageChange}')
     # print(f'{float(totalLoss)}')
-    print(f'Great Increase in Profits: {str(maxProfitDate)} (${float(maxProfit)})')
+    #print(f'Great Increase in Profits: {str(maxProfitDate)} (${float(maxProfit)})')
 
     #print(f'{float(maxProfit)}')
-    print(f'Greatest Decrease in Profits: {str(maxLossDate)} (${float(maxLoss)})')
+   # print(f'Greatest Decrease in Profits: {str(maxLossDate)} (${float(maxLoss)})')
     #print(f'{float(maxLoss)}')
 
 
