0c8ee8220f454e9c5cb18e7af13be2c772ccf35c
lisatian8 <lt8760@yahoo.com>
2019-05-09 15:18:48 -0400

    main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,7 @@
 # Modules
 import os
 import csv
+#import math
 
 # Prompt user for video lookup
 #video = input("What show or movie are you looking for? ")
@@ -10,32 +11,75 @@ dates = []
 profitLosses = []
 
 totalNumOfMonths = 0
-totalPrfitLost = 0
+totalPrfitLoss = 0
 maxLoss = 0
+maxLossDate = ""
 maxProfit = 0
+maxProfitDate = ""
+totalLoss = 0
+averageChange = 0
+
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join(".", "Resources", "election_data.csv")
 
 # Bonus
 # ------------------------------------------
 # Set variable to check if we found the video
-found = False
+#found = False
 
 # Open the CSV
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     # Loop through looking for the video
     for row in csvreader:
 
         dates.append(str(row[0]))
-        print("here")
-        print(str(row[0]))
-        #print(f'date: {row[0]}')
+       # print("here")
+        #print(str(row[0]))
+        ####print(f'date: {row[0]}')
+
+        profitLosses.append(float(row[1]))
+       # print("here")
+        #print(str(row[0]))
+        ####print(f'profigLosses: {row[1]}')
+
+        totalPrfitLoss = float(totalPrfitLoss) + float(row[1])
+
+        #if float(row[1]) > 0 && float(row[1]) > float(maxProfit):
+        if float(row[1]) > float(maxProfit):
+            maxProfit = float(row[1])
+            maxProfitDate = str(row[0])
+
+        elif float(row[1]) < float(maxLoss):
+            maxLoss = float(row[1])
+            maxLossDate = str(row[0])
+            totalLoss = totalLoss + float(row[1])
+
+    #print(dates)
+    #print(profitLosses)
+    #print(f"{len(dates)}")
+
+    totalNumOfMonths=len(dates)
+    averageChange = float(totalPrfitLoss)/float(totalNumOfMonths)
+    #averageChange = float(totalLoss)/float(totalNumOfMonths)
+    
+
+    print(f'Total Months: {totalNumOfMonths}')
+    print(f'Total: ${totalPrfitLoss}')
+    print(f'Average Change: ${averageChange}')
+    # print(f'{float(totalLoss)}')
+    print(f'Great Increase in Profits: {str(maxProfitDate)} (${float(maxProfit)})')
+
+    #print(f'{float(maxProfit)}')
+    print(f'Greatest Decrease in Profits: {str(maxLossDate)} (${float(maxLoss)})')
+    #print(f'{float(maxLoss)}')
+
+
 
         #print(row[0])
         #if row[0] == video:
