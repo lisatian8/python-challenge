d82c76c9281ebce9aa27d34779def0d413d3a416
lisatian8 <lt8760@yahoo.com>
2019-05-11 01:12:33 -0400

    main.py last commit for pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,15 +9,19 @@ import csv
 # Lists to store data
 dates = []
 profitLosses = []
+changes = []
 
 totalNumOfMonths = 0
 totalPrfitLoss = 0
 maxLoss = 0
 maxLossDate = ""
-maxProfit = 0
-maxProfitDate = ""
+maxGain = 0
+maxGainDate = ""
 totalLoss = 0
 averageChange = 0
+totalChanges = 0
+count = 0
+currentChange = 0
 
 
 # Set path for file
@@ -51,46 +55,85 @@ with open(csvpath, newline="") as csvfile:
         totalPrfitLoss = float(totalPrfitLoss) + float(row[1])
 
         #if float(row[1]) > 0 && float(row[1]) > float(maxProfit):
-        if float(row[1]) > float(maxProfit):
-            maxProfit = float(row[1])
-            maxProfitDate = str(row[0])
 
-        elif float(row[1]) < float(maxLoss):
-            maxLoss = float(row[1])
-            maxLossDate = str(row[0])
-            totalLoss = totalLoss + float(row[1])
+
+        if count <= 0:
+             count = int(count) +1
+        else:     
+            currentChange = float(row[1]) - float(profitLosses[int(count)-1])    
+            totalChanges = float(totalChanges) + float(row[1]) - float(profitLosses[int(count)-1])
+            changes.append(currentChange)
+
+            if float(currentChange) > float(maxGain):
+                 maxGain = float(currentChange)
+                 maxGainDate = str(row[0])
+
+            elif float(currentChange) < float(maxLoss):
+                  maxLoss = float(currentChange)
+                  maxLossDate = str(row[0])
+                  totalLoss = totalLoss + float(row[1])
+            
+            count = int(count) + 1
+             
+
 
     #print(dates)
     #print(profitLosses)
     #print(f"{len(dates)}")
+    ####print(f'{totalChanges}')
 
     totalNumOfMonths=len(dates)
-    averageChange = float(totalPrfitLoss)/float(totalNumOfMonths)
+    #averageChange = float(totalChanges/float(len(totalChanges)))
     #averageChange = float(totalLoss)/float(totalNumOfMonths)
+    averageChange = float(totalChanges)/float(totalNumOfMonths -1)
     
-
+    #### print on terminal
+    print("Financial Analysis")
+    print("----------------------------")
     print(f'Total Months: {int(totalNumOfMonths)}')
     print(f'Total: ${int(totalPrfitLoss)}')
-    print(f'Average Change: ${int(averageChange)}')
+    print(f'Average Change: ${float(averageChange)}')
     # print(f'{float(totalLoss)}')
-    print(f'Great Increase in Profits: {str(maxProfitDate)} (${float(maxProfit)})')
+    print(f'Great Increase in Profits: {str(maxGainDate)} (${round(float(maxGain))})')
 
     #print(f'{float(maxProfit)}')
-    print(f'Greatest Decrease in Profits: {str(maxLossDate)} (${float(maxLoss)})')
+    print(f'Greatest Decrease in Profits: {str(maxLossDate)} (${round(float(maxLoss))})')
     #print(f'{float(maxLoss)}')
 
+   # print(changes)
 
 
-        #print(row[0])
-        #if row[0] == video:
-          #  print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+#### write to file
+# Specify the file to write to
+#output_path = os.path.join(".", "output", "pypoll_output.txt")
 
-            # BONUS: Set variable to confirm we have found the video
-          #  found = True
+f = open('./output/pybank_output.txt', 'w')
 
-            # BONUS: Stop at first results to avoid duplicates
-           # break
+f.write('\nFinancial Analysis')
+f.close()
 
-    # If the video is never found, alert the user
-    #if found is False:
-       # print("Sorry about this, we don't seem to have what you are looking for!")
+f = open('./output/pybank_output.txt', 'a')
+#f.write('\n')
+f.write('\n----------------------------')
+f.write('\n')
+f.write('Total Months: : ')
+f.write(str(totalNumOfMonths))
+f.write('\n')
+f.write('Total: $')
+f.write(str(round(totalPrfitLoss)))
+f.write('\n')
+f.write('Average Change: $')
+f.write(str(averageChange))
+f.write('\n')
+f.write('Great Increase in Profits: ')
+f.write(maxGainDate)
+f.write(' $(')
+f.write(str(round(maxGain)))
+f.write(')\n')
+f.write('Greatest Decrease in Profits: ')
+f.write(maxGainDate)
+f.write(' $(')
+f.write(str(round(maxLoss)))
+f.write(')\n')
+f.close()
+
