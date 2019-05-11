1628977d5fdd66148959ae5a0ba399330240fcfb
lisatian8 <lt8760@yahoo.com>
2019-05-10 23:41:36 -0400

    PyPoll last commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,18 +10,13 @@ import csv
 candidates = ["Correy", "Khan", "Li", "O'Tooley"]
 votes = [0,0,0,0]
 percentages=[]
-
 ranking = []
-
 totalVotes = 0
-#totalPrfitLoss = 0
 maxVote = 0
 winner = ""
 
-
 # Set path for file
 csvpath = os.path.join(".", "Resources", "election_data.csv")
-
 
 
 # Open the CSV
@@ -38,10 +33,8 @@ with open(csvpath, newline="") as csvfile:
 
        votes[int(candidates.index(str(row[2])))] = votes[int(candidates.index(str(row[2])))]  + 1
 
-
     ####print(candidates)
     ####print(votes)
-
     
 
    #### print(f'Total Votes {totalVotes}')
@@ -52,16 +45,17 @@ with open(csvpath, newline="") as csvfile:
       if vote > maxVote:
          maxVote = vote
     
-    winner = candidates[votes.index(maxVote)]
+winner = candidates[votes.index(maxVote)]
     ####print(f'{maxVote}')
     #ranking.append(str(winner)
     ####print(f'{winner}')
     ####print(percentages)
     ####print(ranking)
     #sort votes in decending
-    ####ranking.sort(reverse = True)
+ranking.sort(reverse = True)
     ####print(ranking)
 
+#### print to screen
 print("Election Results")
 print("------------------------")
 print(f'Total Votes: {totalVotes}')
@@ -73,4 +67,35 @@ print("------------------------")
 print(f'Winner: {winner}')
 print("------------------------")
 
+#### write to file
+# Specify the file to write to
+#output_path = os.path.join(".", "output", "pypoll_output.txt")
 
+f = open('./output/pypoll_output.txt', 'w')
+
+f.write('\nElection Results')
+f.close()
+
+f = open('./output/pypoll_output.txt', 'a')
+f.write('\n')
+f.write('------------------------')
+f.write('\n')
+f.write('Total Votes: ')
+f.write(str(totalVotes))
+f.write('\n------------------------')
+f.write('\n')
+for rank in ranking:
+  f.write(candidates[int(votes.index(rank))])
+  f.write(': ')
+  f.write(str(round(percentages[int(votes.index(rank))], 3)))
+  f.write('% (')
+  f.write(str(rank))
+  f.write(')')
+  f.write('\n')
+f.write('------------------------')
+f.write('\n')
+f.write('winner: ')
+f.write(winner)
+f.write('\n------------------------')
+f.write('\n')
+f.close()
