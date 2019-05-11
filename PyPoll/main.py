7e3156a04aa1132575c231fe8c4ee449fc93fafa
lisatian8 <lt8760@yahoo.com>
2019-05-10 22:11:22 -0400

    main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,24 +9,20 @@ import csv
 # Lists to store data
 candidates = ["Correy", "Khan", "Li", "O'Tooley"]
 votes = [0,0,0,0]
+percentages=[]
+
+ranking = []
 
 totalVotes = 0
 #totalPrfitLoss = 0
-#maxLoss = 0
-#maxLossDate = ""
-#maxProfit = 0
-#maxProfitDate = ""
-#totalLoss = 0
-#averageChange = 0
+maxVote = 0
+winner = ""
 
 
 # Set path for file
 csvpath = os.path.join(".", "Resources", "election_data.csv")
 
-# Bonus
-# ------------------------------------------
-# Set variable to check if we found the video
-#found = False
+
 
 # Open the CSV
 with open(csvpath, newline="") as csvfile:
@@ -42,58 +38,39 @@ with open(csvpath, newline="") as csvfile:
 
        votes[int(candidates.index(str(row[2])))] = votes[int(candidates.index(str(row[2])))]  + 1
 
-       # print("here")
-        #print(str(row[0]))
-        ####print(f'date: {row[0]}')
 
-       # profitLosses.append(float(row[1]))
-       # print("here")
-        #print(str(row[0]))
-        ####print(f'profigLosses: {row[1]}')
+    ####print(candidates)
+    ####print(votes)
 
-        
-
-        #if float(row[1]) > 0 && float(row[1]) > float(maxProfit):
-       # if float(row[1]) > float(maxProfit):
-           # maxProfit = float(row[1])
-          #  maxProfitDate = str(row[0])
-
-        #elif float(row[1]) < float(maxLoss):
-            #maxLoss = float(row[1])
-           # maxLossDate = str(row[0])
-           # totalLoss = totalLoss + float(row[1])
-
-    print(candidates)
-    print(votes)
-    #print(f"{len(dates)}")
-
-    #totalNumOfMonths=len(dates)
-    #averageChange = float(totalPrfitLoss)/float(totalNumOfMonths)
-    #averageChange = float(totalLoss)/float(totalNumOfMonths)
     
 
-    print(f'Total Votes {totalVotes}')
-    #print(f'Total: ${totalPrfitLoss}')
-    #print(f'Average Change: ${averageChange}')
-    # print(f'{float(totalLoss)}')
-    #print(f'Great Increase in Profits: {str(maxProfitDate)} (${float(maxProfit)})')
+   #### print(f'Total Votes {totalVotes}')
 
-    #print(f'{float(maxProfit)}')
-   # print(f'Greatest Decrease in Profits: {str(maxLossDate)} (${float(maxLoss)})')
-    #print(f'{float(maxLoss)}')
+    for vote in votes:
+      ranking.append(vote)
+      percentages.append(float((vote/totalVotes)*100))
+      if vote > maxVote:
+         maxVote = vote
+    
+    winner = candidates[votes.index(maxVote)]
+    ####print(f'{maxVote}')
+    #ranking.append(str(winner)
+    ####print(f'{winner}')
+    ####print(percentages)
+    ####print(ranking)
+    #sort votes in decending
+    ####ranking.sort(reverse = True)
+    ####print(ranking)
+
+print("Election Results")
+print("------------------------")
+print(f'Total Votes: {totalVotes}')
+print("------------------------")
+####print(ranking)
+for rank in ranking:
+  print(f'{candidates[int(votes.index(rank))]}:  {round(percentages[int(votes.index(rank))], 3)}%  ({rank})')
+print("------------------------")
+print(f'Winner: {winner}')
+print("------------------------")
 
 
-
-        #print(row[0])
-        #if row[0] == video:
-          #  print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
-
-            # BONUS: Set variable to confirm we have found the video
-          #  found = True
-
-            # BONUS: Stop at first results to avoid duplicates
-           # break
-
-    # If the video is never found, alert the user
-    #if found is False:
-       # print("Sorry about this, we don't seem to have what you are looking for!")
